685c0cbe7d0980221756006107a0226e840fde0b
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-09-23 22:56:53 -0300

    added incomplete
    Code works for sample input; however, it's not accepted at spoj. yet.

--- lcbm.c
+++ lcbm.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(void){
-
+    short testCases;
+    scanf("%hi", &testCases);
+    while(testCases--){ 
+        short nodes;
+        scanf("%hi", &nodes);
+        int bst[nodes], index = 0, counter = 0;
+        memset(bst, 0, sizeof(int) * nodes);
+        for(index = 0; index < nodes; index++){
+            scanf("%d", &bst[index]);
+        }
+        for(index = 0; index < nodes; index++){
+            if (bst[index] == -1)
+                counter++;
+        }
+        (nodes % 4 < counter) ? printf("F\n") : printf("T\n");
+        memset(bst, 0, sizeof(int) * nodes);
+    }
     return 0;
 }
